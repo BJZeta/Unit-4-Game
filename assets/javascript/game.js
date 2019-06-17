278492e49df31fdc28dc667930ba9372d5a8b190
Brandon U <unzueta.brandon@gmai.com>
2019-06-16 19:31:26 -0700

    made the win/loss counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
-    var randomNum = Math.floor((100*Math.random())+10);
-    var purple = Math.floor((5*Math.random()) + 1);
-    var red = Math.floor(Math.random() * 5);
-    var yellow = Math.floor(Math.random() * 20);
-    var blue = Math.floor(Math.random() * 10);
+    var randomNum = Math.floor((100 * Math.random()) + 10);
+    var purple = Math.floor((5 * Math.random()) + 1);
+    var red = Math.floor((5 * Math.random()) + 1);
+    var yellow = Math.floor((8 * Math.random()) + 1);
+    var blue = Math.floor((8 * Math.random()) + 1);
     var playerScore = 0;
-    
-    $("#random-number").append(randomNum);
+    var wins = 0;
+    var loses = 0;
+    var reset = function () {
+        randomNum = Math.floor((100 * Math.random()) + 10);
+        purple = Math.floor((5 * Math.random()) + 1);
+        red = Math.floor((5 * Math.random()) + 1);
+        yellow = Math.floor((8 * Math.random()) + 1);
+        blue = Math.floor((8 * Math.random()) + 1);
+        playerScore = 0;
+        $("#total-score").empty(Text);
+        $("#total-score").text(playerScore);
+        $("#random-number").empty(Text);
+        $("#random-number").text(randomNum);
+    }
+
+    $("#random-number").text(randomNum);
+    $("#wins").text("Wins: " + wins);
+    $("#loses").text("Loses: " + loses);
     $("#purple-crystal").attr("value", purple);
     $("#red-crystal").attr("value", red);
     $("#blue-crystal").attr("value", blue);
@@ -14,35 +30,57 @@ $(document).ready(function () {
     // $("#total-score").attr("value", playerScore);
     $("#total-score").text(playerScore);
 
-    $("#purple-crystal").on("click", function(){
+    $("#purple-crystal").on("click", function () {
         playerScore += purple;
         console.log(playerScore);
         $("#total-score").text(playerScore);
+        checkScore();
     });
 
-    $("#red-crystal").on("click", function(){
+    $("#red-crystal").on("click", function () {
         playerScore += red;
         console.log(playerScore);
         $("#total-score").text(playerScore);
+        checkScore();
     });
 
-    $("#blue-crystal").on("click", function(){
+    $("#blue-crystal").on("click", function () {
         playerScore += blue;
         console.log(playerScore);
         $("#total-score").text(playerScore);
+        checkScore();
     });
 
-    $("#yellow-crystal").on("click", function(){
+    $("#yellow-crystal").on("click", function () {
         playerScore += yellow;
         console.log(playerScore);
         $("#total-score").text(playerScore);
+        checkScore();
     });
 
 
+    // if (playerScore > randomNum) {
+    //     loses = loses + 1;
+    //     $("#loses").text("Loses: " + loses);
+    //     console.log("hello")
+    // }
+
+    var checkScore = function () {
+        if (playerScore > randomNum) {
+            loses = loses + 1;
+            $("#loses").text("Loses: " + loses);
+            reset();
+        } else if (playerScore === randomNum) {
+            wins = wins + 1;
+            $("#wins").text("Wins: " + wins);
+            reset();
+        }
+    }
 
 
-     
 
 
-    
+
+
+
 });
